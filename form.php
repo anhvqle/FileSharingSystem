2f624750030c2d545502ba8faf5e3d0a13f65af1
Anh Le <a.v.le@wustl.edu>
2020-09-28 04:07:19 +0700

    Test

--- form.php
+++ form.php
@@ -5,14 +5,16 @@
     <title></title>
   </head>
   <body>
-    <?php
     <form enctype="multipart/form-data" action="uploader.php" method="POST">
     <p>
       <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
       <label for="uploadfile_input">Choose a file to upload:</label> <input name="uploadedfile" type="file" id="uploadfile_input" />
     </p>
-    <p><input type="submit" value="Upload File" /></p>
+    <p>
+      <input type="submit" value="Upload File" />
+    </p>
     </form>
+    <?php
     ?>
   </body>
 </html>
