627984cbeb38e55ad4b0533c36a42bc03eda6963
Anh Le <a.v.le@wustl.edu>
2020-09-28 04:24:03 +0700

    Self Submitting Forms

--- anhvqle/form.php
+++ anhvqle/form.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <h1>Welcome Back, Anh Le!</h1>
-    <form enctype="multipart/form-data" action="uploader.php" method="POST">
+    <form enctype="multipart/form-data" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
     <p>
       <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
       <label for="uploadfile_input">Choose a file to upload:</label> <input name="uploadedfile" type="file" id="uploadfile_input" />
@@ -16,7 +16,11 @@
     </p>
     </form>
     <?php
-
+    if(isset($_POST['uploadedfile'])){
+        printf("<p><strong>%s</strong></p>\n",
+          htmlentities($_POST['uploadedfile'])
+        );
+    }
     ?>
   </body>
 </html>
