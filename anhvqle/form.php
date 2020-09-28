f88ce64958ee7a2cd8fc2e6f7ad7871e5424526d
Kien Ta <talekien1710@gmail.com>
2020-09-29 02:58:34 +0700

    minor changes

--- anhvqle/form.php
+++ anhvqle/form.php
@@ -20,7 +20,7 @@
         printf("<p><strong>%s</strong></p>\n",
           htmlentities($_POST['uploadedfile'])
         );
-        echo(htmlentities($_POST['uploadedfile']);
+        echo($_POST['uploadedfile']);
     }
     ?>
   </body>
