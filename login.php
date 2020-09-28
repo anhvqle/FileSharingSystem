ec03c3a63bdca4db64b9f5682adcc9aba01615f6
Kien Ta <talekien1710@gmail.com>
2020-09-29 03:13:02 +0700

    change to same post page

--- login.php
+++ login.php
@@ -4,15 +4,57 @@ $username = trim($username);
 //echo("$username");
 
 $file = fopen("users.txt", "r");
+$userDetected = false 
 
 while(!feof($file) ){
     if($username == trim(fgets($file))){
-         //printf("Username Found");
-         header("Location: $username/form.php");
-         exit;
+          $userDetected = true
+     //     header("Location: $username/form.php");
+     //     exit;
     }
  }
- header("Location: userNotFound.html");
+ fclose($file);
 
-fclose($file);
+
+
+
+ if($userDetected){
+?>
+     <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Welcome Back <?php $username ?></h1>
+    <form enctype="multipart/form-data" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+    <p>
+      <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
+      <label for="uploadfile_input">Choose a file to upload:</label> <input name="uploadedfile" type="file" id="uploadfile_input" />
+    </p>
+    <p>
+      <input type="submit" value="Upload File" />
+    </p>
+    </form>
+    <?php
+    if(isset($_POST['uploadedfile'])){
+        printf("<p><strong>%s</strong></p>\n",
+          htmlentities($_POST['uploadedfile'])
+        );
+        echo($_POST['uploadedfile']);
+    }
+    ?>
+  </body>
+</html>
+
+
+
+
+<?php
+ }
+ else{
+     header("Location: userNotFound.html");
+ }
+
 ?>
