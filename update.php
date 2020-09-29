f2e03477c00424b3f77c879b6097474ae870e7a4
Kien Ta <talekien1710@gmail.com>
2020-09-29 08:06:05 +0700

    update finished

--- update.php
+++ update.php
@@ -7,7 +7,15 @@
         $filepath = "$username/$filename";
 
      if( move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $filepath) ){
-          echo("upload_success");
+        ?>
+        <body>
+        <form action="display.php" method="post">
+                <p>Update success: Click to direct to your file system</p>
+                <input type="hidden" name="username" value = <?php echo($username); ?>>
+                <input type="submit" value="Display">
+            </form>  
+        </body>
+<?php
           exit;
      }else{
           echo("upload_failure");
@@ -18,10 +26,3 @@
     }
 ?>
 
-<body>
-          <form action="display.php" method="post">
-          <p>Update success: Click to direct to your file system</p>
-          <input type="hidden" name="username" value = <?php echo($username); ?>>
-          <input type="submit" value="Display">
-        </form>  
-</body>
