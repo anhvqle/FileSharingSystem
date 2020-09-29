5fc85eded55e1c98a0826b7613d720875e21497a
Kien Ta <talekien1710@gmail.com>
2020-09-29 07:54:40 +0700

    minor change

--- login.php
+++ login.php
@@ -6,17 +6,6 @@ $username = trim($username);
 $file = fopen("users.txt", "r");
 $userDetected = false;
 
-function httpPost($url, $data)
-{
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
-}
-
 while(!feof($file) ){
     if($username == trim(fgets($file))){
           $userDetected = true;
