a95f7e46603eab0fcd3ef2f3448a81bd28d51d57
Kien Ta <talekien1710@gmail.com>
2020-09-29 07:46:53 +0700

    more structured code added

---
+++ display.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+     
+    <meta charset="utf-8">
+    <title><?php echo(htmlentities($username)); ?></title>
+    
+  </head>
+
+  <body>
+ 
+    <h1>Welcome Back <?php echo(htmlentities($username)); ?></h1>
+    <?php
+          // Display all files
+          $files = array_diff(scandir($username), array('.', '..'));
+          // print_r($files);
+          foreach($files as $file){
+               //echo $file;
+               $dir = "$username/$file";
+               // echo(htmlentities($dir));
+               ?>         
+                    Filename: <?php echo(htmlentities($file)) ?>
+                    <form action="<?php echo(htmlentities($dir)) ?>" method="post">
+                         <input type="submit" value="See file">
+                         <input type="hidden" name="username" value = <?php echo($username); ?>>
+                    </form>
+                    <form action="delete.php" method="post">
+                         <input type="submit" value="Delete">
+                         <input type="hidden" name="username" value = <?php echo($username); ?>>
+                         <input type="hidden" name="delete" value = <?php echo(htmlentities($file)); ?>>
+                    </form>
+     <?php     
+          }
+    ?>
+    <form enctype="multipart/form-data" action = "update.php" method="post">
+    <p>
+      <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
+      <label for="uploadfile_input">Choose a file to upload:</label> <input name="uploadedfile" type="file" id="uploadfile_input" />
+    </p>
+    <input type="hidden" name="username" value = <?php echo($username); ?>>
+    <p>
+      <input type="submit" name="submit" value="Upload File" />
+    </p>
+    </form>
+    <?php
+//     echo($_POST['uploadedfile']);
+    if(isset($_POST['submit'])){
+     //    echo($_FILES['uploadedfile']['name']);
+     //    echo($_FILES['uploadedfile']['type']);
+     //    echo($_FILES['uploadedfile']['size']);
+     //    echo($_FILES['uploadedfile']['tmp_name']);
+
+        $filename = basename($_FILES['uploadedfile']['name']);
+        $filepath = "$username/$filename";
+
+     if( move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $filepath) ){
+          echo("upload_success");
+          exit;
+     }else{
+          echo("upload_failure");
+          
+          exit;
+     }
+
+    }
+    ?>
+  </body>
+</html>
+
+
+
